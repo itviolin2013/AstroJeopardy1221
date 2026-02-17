35aaba05d1a72f64ff83bbcf88a15c43b386bb7d
evalenta <valenta.6@osu.edu>
2026-02-16 20:41:32 -0500

    started the play function as both a .py and a .ipynb to make sure it works

---
+++ Play_Function.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+catagories = ["exoplanets", "blackholes", "galaxies", "stars", "solar system"]
+
+def play():
+    print("please select a catagory: exoplanets, blackholes, galaxies, stars, solar system")
+    category = input()
+
+    if category == "exoplanets":
+        print("select point value: 100, 200, 300, 400, 500")
+        point_value = input()
+        if point_value == "100":
+
+            return play()
+            
+
+
+
+
+
+
+    
+
+
