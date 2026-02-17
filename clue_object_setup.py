c3a798aa122f2d6fcb0bb348ab1b0c0bd7059d1c
itviolin2013 <isabelletardivon@yahoo.com>
2026-02-16 20:52:02 -0500

    Created generate_tiles() function

--- clue_object_setup.py
+++ clue_object_setup.py
@@ -18,18 +18,18 @@ class Tile:
     def __lt__(self, other):
         return self.pointValue < other.pointValue
         
-
-board = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24], [25, 26, 27, 28, 29, 30]])
-
-
-i = 0
-for row in board:
-    for element in row: 
-        i += 1  # or i += 0 if you really just want a no-op; i must be defined first
         
+def generate_tiles(clues, answers, pointValue, isDD, beenUsed):
+    board = np.array([[1, 2, 3, 4, 5, 6], 
+                      [7, 8, 9, 10, 11, 12], 
+                      [13, 14, 15, 16, 17, 18], 
+                      [19, 20, 21, 22, 23, 24], 
+                      [25, 26, 27, 28, 29, 30]])
+    for row in range(5):
+        for column in range(6):
+            board[row, column] = Tile(clues[row, column], answers[row, column], pointValue[row, column], isDD[row, column], beenUsed[row, column])
+    print(board[0, 0].answer)
+    print(board)
 
-tile1 = Tile("hello", "goodbye", True, 100, False)
-print(tile1)
 
-board = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24], [25, 26, 27, 28, 29, 30]])
 
