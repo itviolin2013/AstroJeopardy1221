3d6460d369eb8ff3beb030f3df3b515c9f6abe01
evalenta <valenta.6@osu.edu>
2026-02-16 21:31:46 -0500

    Made the play.py little guys actually work YAY what a great feat am I right?!

--- Play_Function.py
+++ Play_Function.py
@@ -1,21 +1,24 @@
 import numpy as np
+from AI_Setup import prompt_llm
 
-catagories = ["exoplanets", "blackholes", "galaxies", "stars", "solar system"]
+
+categories = ["exoplanets", "blackholes", "galaxies", "stars", "solar system"]
+point_values = ["100", "200", "300", "400", "500"]
 
 def play():
-    print("please select a catagory: exoplanets, blackholes, galaxies, stars, solar system")
-    category = input()
+    category = input("Enter your catagory: exoplanets, blackholes, galaxies, stars, solar system")
 
-    if category == "exoplanets":
-        print("select point value: 100, 200, 300, 400, 500")
-        point_value = input()
-        if point_value == "100":
+    if category not in categories:
+        category = input("Please select a valid category: exoplanets, blackholes, galaxies, stars, solar system")
 
-            return play()
-            
+    if category in categories:
+        point_value = input("select point value: 100, 200, 300, 400, 500")
 
-
-
+        if point_value not in point_values:
+            point_value = input("Please select a valid point value: 100, 200, 300, 400, 500")
+   
+    return point_value, category
+    
 
 
 
