f76a186a2218447599d054525c5fda122ca7c020
evalenta <valenta.6@osu.edu>
2026-02-17 20:04:47 -0500

    made play into int

--- Play_Function.py
+++ Play_Function.py
@@ -6,16 +6,16 @@ categories = ["exoplanets", "blackholes", "galaxies", "stars", "solar system"]
 point_values = ["100", "200", "300", "400", "500"]
 
 def play():
-    category = input("Enter your catagory: exoplanets, blackholes, galaxies, stars, solar system")
+    category = int(input("Enter your catagory: exoplanets, blackholes, galaxies, stars, solar system"))
 
     if category not in categories:
-        category = input("Please select a valid category: exoplanets, blackholes, galaxies, stars, solar system")
+        category = int(input("Please select a valid category: exoplanets, blackholes, galaxies, stars, solar system"))
 
     if category in categories:
-        point_values = input("select point value: 100, 200, 300, 400, 500")
+        point_values = int(input("select point value: 100, 200, 300, 400, 500"))
 
         if point_values not in point_values:
-            point_values = input("Please select a valid point value: 100, 200, 300, 400, 500")
+            point_values = int(input("Please select a valid point value: 100, 200, 300, 400, 500"))
 
     return point_values, category
 
